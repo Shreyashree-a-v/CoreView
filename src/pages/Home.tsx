3240ffcbc39fee64cae86bcd36b36e43dd69a9f9
= <=>
2026-02-06 11:19:17 +0530

    Added tailwind library

---
+++ src/pages/Home.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className="min-h-screen bg-blue-600 flex items-center justify-center">
+      <div className="bg-white px-8 py-4 rounded-xl text-2xl font-bold">
+        Tailwind v4 works 🚀
+      </div>
+    </div>
+  )
+}
